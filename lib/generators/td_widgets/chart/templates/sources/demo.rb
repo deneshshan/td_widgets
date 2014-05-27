4e18b78d6c2fa0b211608beba09ac40380d03a1b
Denesh Shan <denesh.shan@gmail.com>
2014-05-27 18:48:09 +0100

    made chart render hello world. made edit for for chart.

--- lib/generators/td_widgets/chart/templates/sources/demo.rb
+++ lib/generators/td_widgets/chart/templates/sources/demo.rb
@@ -1,6 +1,13 @@
 module Sources 
   module Chart
     class Demo < Sources::Chart::Base
+      def available?
+        true
+      end
+
+      def get(options = {})
+        { :value => rand(2) == 1 }
+      end
     end
   end
 end
