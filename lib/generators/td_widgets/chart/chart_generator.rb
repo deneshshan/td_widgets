492b2915d9f7559821b4351e5ed4ef9ccb9e4857
Denesh Shan <denesh.shan@gmail.com>
2014-05-28 18:08:14 +0100

    Moved generating assets to the end of file

--- lib/generators/td_widgets/chart/chart_generator.rb
+++ lib/generators/td_widgets/chart/chart_generator.rb
@@ -6,11 +6,6 @@ module TdWidgets
 
       source_root File.expand_path('../templates', __FILE__)
 
-      def copy_assets
-        puts "Copying assets..."
-        copy_file '../../../../../vendor/assets/javascripts/chart/angular-charts.min.js', get_vendor_assets_js_path + 'angular-charts.min.js'
-        copy_file '../../../../../vendor/assets/javascripts/chart/d3.min.js', get_vendor_assets_js_path + 'd3.min.js' 
-      end
 
       def create_widget_assets
         puts "Creating widget assets..."
@@ -31,6 +26,13 @@ module TdWidgets
         puts "Creating demo source..."
         copy_file "sources/demo.rb", get_sources_path + "chart/demo.rb"
       end
+
+      def copy_assets
+        puts "Copying assets..."
+        copy_file '../../../../../vendor/assets/javascripts/chart/angular-charts.min.js', get_vendor_assets_js_path + 'angular-charts.min.js'
+        copy_file '../../../../../vendor/assets/javascripts/chart/d3.min.js', get_vendor_assets_js_path + 'd3.min.js' 
+      end
+
     end
   end
 end
